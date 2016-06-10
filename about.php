00d6890f80f509e951a1a671ecfb915ad4618ff5
James Johnson <tripp.johnson4934@gmail.com>
2016-06-10 17:38:29 +0000

    first commit

--- about.php
+++ about.php
@@ -44,10 +44,9 @@ window.onload = window.onresize = function () {
 
 <main>
 
-<!-- These sections can be repeated as many times as nessisary to organize information -->
+<!-- These sections can be repeated as many times as nessisary to organize information(15yy here to copy) -->
 <section class="info">
 
-<div>
 
 <!-- This is the main picture for the page -->
 <img class="left" src="static/media/projectImages/acc_sign.jpg"/>
@@ -56,11 +55,9 @@ window.onload = window.onresize = function () {
 <p class="right">Project Olorun is a multidisciplinary effort to create a solar recharge station to help provide electricity to  communities that are too remote to facilitate a hardwired solution. </p>
 
 <!-- This is the black line that is at the bottom --> 
-<hr>
-</div>
 </section>
+<hr>
 <section class="info">
-<div>
 <!-- This is the second picture for the page -->
 
 <img class="right" src="static/media/projectImages/acc_sign.jpg"/>
@@ -69,8 +66,8 @@ window.onload = window.onresize = function () {
 <p class="left"> some of the diciplines included are: carpentry, electrical engineering, computer science and mathamatics  </p>
 
 <!-- This is the second black line that is at the bottom --> 
-<hr>
 </section>
+<hr>
 </main>
 
 <!-- This is the footer -->
--- info.php
+++ info.php
@@ -44,39 +44,112 @@ window.onload = window.onresize = function () {
 
 <main>
 
-<!-- These sections can be repeated as many times as nessisary to organize information -->
+<!-- These sections can be repeated as many times as nessisary to organize information(15yy here to copy) -->
 <section class="info">
-
-<div>
 
 <!-- This is the main picture for the page -->
 <img class="left" src="static/media/projectImages/acc_sign.jpg"/>
 
 <!-- This is a discription of the website underneath the picture -->
-<p class="right">Project Olorun is a multidisciplinary effort to create a solar recharge station to help provide electricity to  communities that are too remote to facilitate a hardwired solution. </p>
+<section class="right">
+
+<h2>What gave you the idea for Project Olorun?</h2>
+<p>when mr. Johnson arrived at APS in 2014 the school already had green intentions. So he came up with the idea of students being able to use solar energy to charge their laptops and smartphones. From there he came up with an idea for a pergola which would serve both a functional and asthetic purpose.</p>
+</section>
 
 <!-- This is the black line that is at the bottom --> 
-<hr>
-
-</div>
 </section>
 
+<hr>
+
+<!-- These sections can be repeated as many times as nessisary to organize information(19yy here to copy) -->
 <section class="info">
-<div>
-<!-- This is the second picture for the page -->
+
+<!-- This is the main picture for the page -->
 <img class="right" src="static/media/projectImages/acc_sign.jpg"/>
 
-<!-- This is the second discription of the website underneath the picture -->
-<p class="left"> some of the diciplines included are: carpentry, electrical engineering, computer science and mathamatics  </p>
+<!-- This is a discription of the website underneath the picture -->
+<section class="left">
 
-<!-- This is the second black line that is at the bottom --> 
-<hr>
-
-<!-- To repeat, these sections are the main break sections for information -->
-</div>
+<h2>Why did you choose the name Olorun?</h2>
+<p>Mr. Johnson had already been familiar with west african theology and chose Olorun because he was the god of the sky and more specificaly the sun.</p>  
 </section>
 
+<!-- This is the black line that is at the bottom --> 
+</section>
+
+<hr>
+
+<!-- These sections can be repeated as many times as nessisary to organize information(19yy here to copy) -->
+<section class="info">
+
+<!-- This is the main picture for the page -->
+<img class="left" src="static/media/projectImages/acc_sign.jpg"/>
+
+<!-- This is a discription of the website underneath the picture -->
+<section class="right">
+
+<h2>What classes are participating in this project?</h2>
+<p>There is a variety of specialized classes working on the project such as engineering and electrician classes.<p>
+</section>
+
+<!-- This is the black line that is at the bottom --> 
+</section>
+
+<hr>
+
+<!-- These sections can be repeated as many times as nessisary to organize information(19yy here to copy) -->
+<section class="info">
+
+<!-- This is the main picture for the page -->
+<img class="right" src="static/media/projectImages/acc_sign.jpg"/>
+
+<!-- This is a discription of the website underneath the picture -->
+<section class="left">
+
+<h2>What do you see as the future of this project?</h2>
+<p>It brings awareness to sustainable technology.  Mr. Johnson hopes also make it global to provide electricity to African schools.</p>
+</section>
+
+<!-- This is the black line that is at the bottom --> 
+</section>
+
+<hr>
+
+<!-- These sections can be repeated as many times as nessisary to organize information(19yy here to copy) -->
+<section class="info">
+
+<!-- This is the main picture for the page -->
+<img class="left" src="static/media/projectImages/acc_sign.jpg"/>
+
+<!-- This is a discription of the website underneath the picture -->
+<section class="right">
+
+<h2>What has been the most difficult part of this project?</h2>
+<p>The abstract mathematical process of quantifying data and specing the project has been the hardest part.</p>
+</section>
+
+<!-- This is the black line that is at the bottom --> 
+</section>
+
+<hr>
+
+<!-- These sections can be repeated as many times as nessisary to organize information(19yy here to copy) -->
+<section class="info">
+
+<!-- This is the main picture for the page -->
+<img class="right" src="static/media/projectImages/acc_sign.jpg"/>
+
+<!-- This is a discription of the website underneath the picture -->
+<section class="left">
+<h2>How do you see this helping your students in the future?</h2>
+<p>It not only helps all the students have a good place to charge their phones. It also gives students hands on experience and a physical representation of what they can achieve if they work together.</p>
+</section>
+
+<!-- This is the black line that is at the bottom --> 
+</section>
 <!-- This is the footer -->
+
 <?php include ("footer.inc"); ?>
 
 </body></html>
